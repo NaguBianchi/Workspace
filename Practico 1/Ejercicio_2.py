de76dc5f69a87e434c8bcb61b01425babb2dd458
s-passalacqua-iresm <69656420+s-passalacqua-iresm@users.noreply.github.com>
2020-08-31 21:11:07 -0300

    Update Ejercicio_2.py

--- Practico 1/Ejercicio_2.py
+++ Practico 1/Ejercicio_2.py
@@ -7,7 +7,8 @@ if result1 == result2:                        #Alpicamos condicional para mostra
     print('Los valores son iguales')
 else:
     result = result1 / result2
-    print('El resultado de la division es: ', result)
+    print('El resultado de la division es: ', result) # Falta evaluacion para verificar cual es el mayor y cual es el menor si num1 > num2 entonces el 
+                                                      # resultado esta bien calculado, pero si es al reves el resul deberia ser igual a result2/result1
 
 """
 Dados dos número enteros, se necesita saber el resultado de dividir el cuadrado del
